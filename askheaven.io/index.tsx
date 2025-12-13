a4ad21cee3a55737031b17d7c45edad9dd78a1ba
heavendeskai <ja@heavendeskai.com>
2025-12-12 20:38:53 -0600

    Update index.tsx

--- askheaven.io/index.tsx
+++ askheaven.io/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
@@ -9,7 +10,7 @@ if (!rootElement) {
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
